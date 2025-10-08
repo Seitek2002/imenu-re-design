a9a791201c95ab701caed58cb4aef0a59d62e675
Seitek2002 <seitekbegaliev@gmail.com>
2025-10-08 23:43:18 +0600

    jmhjh

--- src/app/(home)/[venue]/menu/_components/Goods.tsx
+++ src/app/(home)/[venue]/menu/_components/Goods.tsx
@@ -12,13 +12,16 @@ const Goods: FC<Props> = ({ category }) => {
   console.log(
     goodItems.filter((item) => item.category.categoryName === category)
   );
+  const filteredGoods = goodItems.filter(
+    (item) => item.category.categoryName === category
+  );
 
   return (
     <div className='grid grid-cols-2 gap-2'>
-      {goodItems.filter((item) => item.category.categoryName === category).length > 0 ? (
-        goodItems
-          .filter((item) => item.category.categoryName === category)
-          .map((item: ApiItem) => <GoodItem key={item.id} item={item} />)
+      {filteredGoods.length > 0 ? (
+        filteredGoods.map((item: ApiItem) => (
+          <GoodItem key={item.id} item={item} />
+        ))
       ) : (
         <h1>Пусто</h1>
       )}
