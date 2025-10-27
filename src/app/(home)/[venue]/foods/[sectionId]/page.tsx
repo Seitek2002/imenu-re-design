fe92c591a66b41c6a2f6bde0136b43a0da2c9aa7
Seitek2002 <seitekbegaliev@gmail.com>
2025-10-27 14:45:41 +0600

    refact

--- src/app/(home)/[venue]/foods/[sectionId]/page.tsx
+++ src/app/(home)/[venue]/foods/[sectionId]/page.tsx
@@ -2,13 +2,14 @@ import Header from '../../_components/Header';
 import Content from './_components/Content';
 
 type PageProps = {
-  searchParams?: {
+  searchParams?: Promise<{
     title?: string;
-  };
+  }>;
 };
 
-const FoodsPage = ({ searchParams }: PageProps) => {
-  const title = (searchParams?.title ?? 'Разделы') as string;
+const FoodsPage = async ({ searchParams }: PageProps) => {
+  const sp = (await searchParams) ?? {};
+  const title = (sp.title ?? 'Разделы') as string;
 
   return (
     <main className='px-2.5 min-h-svh'>
