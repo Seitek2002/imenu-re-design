f619f11ce7fb88e228aeb6ee73810619761cc422
Seitek2002 <seitekbegaliev@gmail.com>
2025-10-29 16:07:12 +0600

    test

--- src/app/(home)/[venue]/history/page.tsx
+++ src/app/(home)/[venue]/history/page.tsx
@@ -1,7 +1,15 @@
 const History = () => {
   return (
-    <div>History page</div>
-  )
-}
+    <main className='min-h-svh px-2.5 bg-[#F8F6F7] flex items-center justify-center'>
+      <div className='bg-white rounded-3xl p-6 text-center shadow-md w-full max-w-sm'>
+        <div className='text-5xl mb-2'>🚧</div>
+        <h1 className='text-xl font-semibold text-[#111111]'>Еще в разработке</h1>
+        <p className='text-[#6B7280] mt-1 text-sm'>
+          Скоро здесь появится история ваших заказов.
+        </p>
+      </div>
+    </main>
+  );
+};
 
-export default History
+export default History;
--- src/app/(home)/[venue]/profile/page.tsx
+++ src/app/(home)/[venue]/profile/page.tsx
@@ -1,7 +1,15 @@
 const Profile = () => {
   return (
-    <div>Profile page</div>
-  )
-}
+    <main className='min-h-svh px-2.5 bg-[#F8F6F7] flex items-center justify-center'>
+      <div className='bg-white rounded-3xl p-6 text-center shadow-md w-full max-w-sm'>
+        <div className='text-5xl mb-2'>🧩</div>
+        <h1 className='text-xl font-semibold text-[#111111]'>Еще в разработке</h1>
+        <p className='text-[#6B7280] mt-1 text-sm'>
+          Профиль скоро будет доступен.
+        </p>
+      </div>
+    </main>
+  );
+};
 
-export default Profile
+export default Profile;
