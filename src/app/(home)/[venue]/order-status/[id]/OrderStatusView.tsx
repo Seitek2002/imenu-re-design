0e97659d3c5441b2cc542aaf017f51877e1f5b23
Seitek2002 <seitekbegaliev@gmail.com>
2025-11-02 16:07:42 +0600

    added-new-component

--- src/app/(home)/[venue]/order-status/[id]/OrderStatusView.tsx
+++ src/app/(home)/[venue]/order-status/[id]/OrderStatusView.tsx
@@ -2,11 +2,11 @@
 
 import CurrentStatus from './_components/CurrentStatus';
 import Header from './_components/Header';
-import Item from '@/app/(home)/[venue]/basket/_components/Item';
 import type { CartItem } from '@/store/basket';
 import { useParams } from 'next/navigation';
 import { useOrderByIdV2 } from '@/lib/api/queries';
 import { isApiErrorDetail } from '@/lib/api/types';
+import OrderItems from './_components/OrderItems';
 
 const OrderStatusView = () => {
   const { id } = useParams<{ id: string }>();
@@ -44,34 +44,11 @@ const OrderStatusView = () => {
     <div className='bg-[#F8F6F7] min-h-svh pb-40'>
       <Header />
       <CurrentStatus />
-      <div className=' px-4 pt-3'>
-        <div className='bg-white rounded-[30px] p-3'>
-          <div className='flex justify-between items-center'>
-            <span className='font-semibold text-lg'>Мои заказы</span>
-            <span className='text-[#727272] text-sm'>
-              {orderItemsCount} позиций
-            </span>
-          </div>
-          <div className='content'>
-            {isNotFound ? (
-              <div className='py-10 text-center text-[#80868B]'>
-                <div className='text-base font-semibold text-[#111111]'>
-                  Заказ не найден
-                </div>
-                <div className='text-sm mt-1'>
-                  Такой заказ отсутствует или был удален.
-                </div>
-              </div>
-            ) : (
-              <ul className='divide-y mt-3 divide-[#E7E7E7]'>
-                {itemsFromOrder.map((it) => (
-                  <Item key={it.key} it={it} statusMode />
-                ))}
-              </ul>
-            )}
-          </div>
-        </div>
-      </div>
+      <OrderItems
+        isNotFound={isNotFound}
+        orderItemsCount={orderItemsCount}
+        itemsFromOrder={itemsFromOrder}
+      />
     </div>
   );
 };
---
+++ src/app/(home)/[venue]/order-status/[id]/_components/OrderItems.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+import Item from '../../../basket/_components/Item';
+import { CartItem } from '@/store/basket';
+
+interface IProps {
+  isNotFound: boolean;
+  orderItemsCount: number;
+  itemsFromOrder: CartItem[];
+}
+
+const OrderItems: FC<IProps> = ({
+  isNotFound,
+  orderItemsCount,
+  itemsFromOrder,
+}) => {
+  return (
+    <div className=' px-4 pt-3'>
+      <div className='bg-white rounded-[30px] p-3'>
+        <div className='flex justify-between items-center'>
+          <span className='font-semibold text-lg'>Мои заказы</span>
+          <span className='text-[#727272] text-sm'>
+            {orderItemsCount} позиций
+          </span>
+        </div>
+        <div className='content'>
+          {isNotFound ? (
+            <div className='py-10 text-center text-[#80868B]'>
+              <div className='text-base font-semibold text-[#111111]'>
+                Заказ не найден
+              </div>
+              <div className='text-sm mt-1'>
+                Такой заказ отсутствует или был удален.
+              </div>
+            </div>
+          ) : (
+            <ul className='divide-y mt-3 divide-[#E7E7E7]'>
+              {itemsFromOrder.map((it) => (
+                <Item key={it.key} it={it} statusMode />
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderItems;
