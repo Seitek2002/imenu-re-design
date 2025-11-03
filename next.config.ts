6f7445049d5cfd18b9adee8d4d7a74bd2b2e6d67
Seitek2002 <seitekbegaliev@gmail.com>
2025-11-03 18:29:02 +0600

    changes

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,21 @@ const nextConfig: NextConfig = {
   images: {
     // Fallback for platforms where Next Image optimizer endpoint isn't available
     // Serves static images directly without /_next/image proxy
-    unoptimized: true,
+    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imenu.kg',
+        port: '',
+        pathname: '/media/**',
+      },
+    ],
   },
   // Hard-disable Lightning CSS optimizations to prevent native binary issues on CI
   experimental: {
     optimizeCss: false,
   },
+  
 };
 
 export default nextConfig;
