cac801b090b51365e5e8cadc58014737653d988b
Seitek2002 <seitekbegaliev@gmail.com>
2026-04-07 22:42:30 +0600

    test

--- src/app/[venue]/products/[slug]/components/ProductSheet.tsx
+++ src/app/[venue]/products/[slug]/components/ProductSheet.tsx
@@ -317,8 +317,7 @@ export default function ProductSheet() {
         </button>
 
         {!activeProduct ? (
-          <div className='flex items-center justify-center h-full min-h-75'>
-            <div className='animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900' />
+          <div className=''>
           </div>
         ) : (
           <ProductContent
