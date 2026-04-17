c817803029cbe5ac604fc90d257c53b5d6863bdf
zhusupovnursultan <nursultan.zhusupov@gmail.com>
2026-04-17 12:26:09 +0300

    feat: add utensils selector

--- src/store/checkout.ts
+++ src/store/checkout.ts
@@ -4,8 +4,10 @@ import { persist, createJSONStorage } from 'zustand/middleware';
 interface CheckoutState {
   phone: string;
   address: string;
+  needUtensils: boolean;
   setAddress: (address: string) => void;
   setPhone: (phone: string) => void;
+  setNeedUtensils: (val: boolean) => void;
   reset: () => void;
 }
 
@@ -17,6 +19,8 @@ export const useCheckout = create<CheckoutState>()(
       reset: () => set({ phone: '' }),
       address: '',
       setAddress: (address) => set({ address }),
+      needUtensils: false,
+      setNeedUtensils: (needUtensils) => set({ needUtensils }),
     }),
     {
       name: 'imenu-checkout-storage', // Ключ в localStorage
